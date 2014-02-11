c0b869cc62fd7b2f89a010c9de12296cfc72856e
Arhipkin Ilya <ilya@arhipkin.com>
2014-02-11 22:35:54 +0600

    Home Server
    Free Templates

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: sex
+ *
+ * Created on 11 Февраль 2014 г., 19:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
